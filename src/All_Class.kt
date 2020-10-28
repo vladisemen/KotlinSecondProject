b86a8eb8accb7f93eca81e14241f49866edf55d9
demon777veka2 <demon777veka@gmail.com>
2020-10-28 15:44:08 +0700

    Добавлены файлы All_Class и KotlinSecondProject с пустыми классами

---
+++ src/All_Class.kt
@@ -0,0 +1,8 @@
+class User {
+}
+
+class Resource {
+}
+
+class Function  {
+}
---
+++ src/KotlinSecondProject.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    val user: User
+    val resource: Resource
+    val function: Function
+
+    
+    }
+
+
