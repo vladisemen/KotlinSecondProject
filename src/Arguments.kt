31ead47ab350e103e02a97e7a80786d894c34293
vladisemen <40513696+vladisemen@users.noreply.github.com>
2020-11-01 16:35:44 +0700

    3.2
    создан data class Arguments

---
+++ src/Arguments.kt
@@ -0,0 +1,10 @@
+data class Arguments(val h: Boolean,
+                     val login: String?,
+                     val pass: String?,
+                     val role: String?,
+                     val res: String?,
+                     val ds: String?,
+                     val de: String?,
+                     val vol: String?) {
+    
+}
