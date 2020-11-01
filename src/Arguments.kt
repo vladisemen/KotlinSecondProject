1dd1432fe873cc96d871e554103471c29ac34260
vladisemen <40513696+vladisemen@users.noreply.github.com>
2020-11-01 18:56:11 +0700

    3.3
    создан метод в файле Main.kt который будет доставать значения из аргументов и раскладывать по полям

--- src/Arguments.kt
+++ src/Arguments.kt
@@ -1,4 +1,5 @@
-data class Arguments(val h: Boolean,
+
+data class Arguments(var h: Boolean,
                      val login: String?,
                      val pass: String?,
                      val role: String?,
@@ -6,5 +7,5 @@ data class Arguments(val h: Boolean,
                      val ds: String?,
                      val de: String?,
                      val vol: String?) {
-    
+
 }
--- src/KotlinSecondProject.kt
+++ src/KotlinSecondProject.kt
@@ -1,14 +1,26 @@
-import models.*
-
 fun main(args: Array<String>) {
 
-    val user: User
-    val resource: Resource
-    val function: Fun
+}
 
+fun checkAmountParams(args: Array<String>): Boolean {
+    return (args.size == 0 || args.size == 1 || args.size == 4 || args.size == 8 || args.size == 14)
+}
+
+fun parseValues(args: Array<String>): Arguments {
+    return when (args.size) {
+        4 -> {
+            Arguments(false, args[1], args[3], null, null, null, null, null)
+        }
+        8 -> {
+            Arguments(false, args[1], args[3], args[5], args[7], null, null, null)
+        }
+        14 -> {
+            Arguments(false, args[1], args[3], args[5], args[7], args[9], args[11], args[13])
+        }
+        else -> {
+            Arguments(true, null, null, null, null, null, null, null)
+        }
     }
-fun checkAmountParams(args: Array<String>): Boolean{
-    return (args.size==0||args.size==1||args.size==4||args.size==8||args.size==14)
 }
 
 
