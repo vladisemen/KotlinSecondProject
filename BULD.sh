04bf215b3742e300fb88a8d7fc1f44373f761773
demon777veka2 <demon777veka@gmail.com>
2020-11-05 15:38:57 +0700

    Изменен файл BULD

--- BULD.sh
+++ BULD.sh
@@ -1,2 +1,1 @@
-#Не работает. Нужно дописать код
 kotlinc -include-runtime src/ -d First.jar
--- RUN.sh
+++
@@ -1,5 +0,0 @@
-#Не работает. Нужно дописать код
-echo "Napishite text"
-read text
-java -jar First.jar $text
-sleep 5m
